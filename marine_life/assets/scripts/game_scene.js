eb74d1a20c21c6d21bdb61f35f25af6bcc287b54
Dmitriy Nemykin <ddraudred@gmail.com>
2021-11-21 09:47:52 +0300

    Cards with faces

---
+++ marine_life/assets/scripts/game_scene.js
@@ -0,0 +1,67 @@
+class GameScene extends Phaser.Scene {
+  preload() {
+    this.preloadBackground();
+    this.preloadCards();
+  }
+
+  preloadBackground() {
+    this.load.image('bg', `${config.imagePath}/background.png`);
+  }
+
+  preloadCards() {
+    this.load.image(config.baseCardKey, `${config.imagePath}/card.png`);
+    for(let i = 1; i <= config.cards.length; ++i) {
+      const cardKey = `${config.baseCardKey}${i}`;
+      this.load.image(cardKey, `${config.imagePath}/${cardKey}.png`);
+    }
+  }
+
+  create() {
+    this.createBackgroud();
+    this.createCards();
+  }
+
+  createBackgroud() {
+    this.add.image(this.width() / 2, this.height() / 2, 'bg');
+  }
+
+  createCards() {
+    this.cards = [];
+    const positions = this.getCardsPositions();
+
+    for(let value of config.cards) {
+      for(let i = 0; i < 2; ++i) {
+        this.cards.push(new Card(this, value, positions.pop()));
+      }
+    }
+  }
+
+  width() {
+    return this.sys.game.config.width;
+  }
+
+  height() {
+    return this.sys.game.config.height;
+  }
+
+  getCardsPositions() {
+    const PADDING = 4;
+    const cardTexture = this.textures.get(config.baseCardKey).getSourceImage();
+    const CARD_WITH = cardTexture.width + PADDING;
+    const CARD_HEIGHT = cardTexture.height + PADDING;
+    const OFFSET_X = (this.width() - config.cols * CARD_WITH) / 2;
+    const OFFSET_Y = (this.height() - config.rows * CARD_HEIGHT) / 2;
+
+    const positions = [];
+    for(let row = 0; row < config.rows; ++row) {
+      for(let col = 0; col < config.cols; ++col) {
+        positions.push({
+          x: CARD_WITH * col + OFFSET_X,
+          y: CARD_HEIGHT * row + OFFSET_Y
+        });
+      }
+    }
+
+    return positions;
+  }
+}
--- marine_life/assets/scripts/main.js
+++ marine_life/assets/scripts/main.js
@@ -2,6 +2,11 @@ const config = {
     type: Phaser.AUTO, // webgl or canvas
     width: 1280,
     height: 720,
+    rows: 2,
+    cols: 5,
+    baseCardKey: 'card',
+    cards: [1, 2, 3, 4, 5],
+    imagePath: 'assets/sprites',
     scene: new GameScene()
 };
 
